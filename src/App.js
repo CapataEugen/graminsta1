eb671c1dfd75ac7f348463a6db35493541a767b0
EugenMinerDeDate <eugencapata802@gmail.com>
2022-05-24 17:49:11 +0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import logo from './images/logo.png';
 
 function App() {
   return (
     <div className="App">
-
+      <div className='appHeader'>
+        <img
+          className='appHeaderImage'
+          src={logo} height={50}
+          alt="logo"
+        />
+      </div>
+      <h1>Salutari</h1>
     </div>
   );
 }
